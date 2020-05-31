31ee688a015960099fc1120f9b405d0ce7a4a1c8
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-05-31 12:13:52 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Sanjeev Kumar");
+$("#policyNumber").text("VC62072930");
+$("#startDate").text("31-May-2020");
+$("#endDate").text("30-May-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB08AF3021");
+$("#make").text("MARUTI");
+$("#model").text("ZEN");
+$("#vehicalSubType").text("LX E2");
+$("#engineNo").text("155255");
+$("#chassis").text("592565");
